7780d44996db4e7e6c26eb465ca09e4cb3bab6e2
maweijian945 <krismaatwork@gmail.com>
2015-05-23 22:31:00 +0800

    static

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static/index'
+  root:to => "static#index"
   resources :posts
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
