335c7be9df57e318fbde8a1426d43ac499d0135a
maweijian945 <krismaatwork@gmail.com>
2015-05-26 19:54:28 +0800

    errmsg

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,19 @@ class PostsController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@post = current_user.posts.new(create_post_params)
-		@post.save
-	end
+		if @post.save
+			flash[:notice] = "Post created!"
+		else
+			if @post.errors.any?
+				flash[:error] ||= []
+				@post.errors.full_messages.each do |value|
+					flash[:error] << value
+				end
+			end
+		end
+		redirect_to root_path
+    end
+  
 
 	private
 	def create_post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :content, presence: true
+  validates :date_publish, presence: true
 end
