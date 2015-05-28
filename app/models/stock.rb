6d38daf5afce6f36999a6f218fa1c297c8865308
maweijian945 <krismaatwork@gmail.com>
2015-05-28 12:35:26 +0800

    Yahoo api integrated

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,16 @@
 class Stock < ActiveRecord::Base
 	has_many :watchings
 	has_many :users, through: :watchings
+	
+	def close
+		return YahooFinance.quotes([self.symbol],[:close])[0].close
+	end
 
+	def change
+		return YahooFinance.quotes([self.symbol],[:change_and_percent_change])[0].change_and_percent_change.split[0]
+	end
+
+	def percent_change
+		return YahooFinance.quotes([self.symbol],[:change_and_percent_change])[0].change_and_percent_change.split[2]
+	end
 end
