998d7e4901a3d6d74d42cbd285a9fca1dec932f4
maweijian945 <krismaatwork@gmail.com>
2015-06-02 20:00:28 +0800

    before hstore-lize

--- db/migrate/20150527022441_create_stocks.rb
+++ db/migrate/20150527022441_create_stocks.rb
@@ -9,6 +9,17 @@ class CreateStocks < ActiveRecord::Migration
       t.float :change
       t.float :percent_change
       t.hstore :details
+      t.datetime :details_exp
+      t.hstore :five_d
+      t.datetime :five_d_exp
+      t.hstore :one_m
+      t.datetime :one_m_exp
+      t.hstore :six_m
+      t.datetime :six_m_exp
+      t.hstore :one_y
+      t.datetime :one_y_exp
+      t.hstore :all
+      t.datetime :all_exp
 
       t.timestamps null: false
     end
